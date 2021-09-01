9c9c4920f5558843998acfadea549805624c7c0c
Luiz Pires <ranzinza@ranzinza.dev>
2021-09-01 01:38:36 -0300

    feat: ensure returns 400 if an invalid email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,13 @@
+import { EmailValidator } from './../protocols/email-validator'
 import { Controller } from './../protocols/controller'
 import { badRequest } from './../helpers/http-helper'
 import { MissingParamError } from '../errors/missing-param-error'
 import { HttpRequest, HttpResponse } from './../protocols/http'
+import { InvalidParamError } from '../errors/invalid-param-error '
 export class SignUpController implements Controller {
+  // eslint-disable-next-line no-useless-constructor
+  constructor (private emailValidator: EmailValidator) {}
+
   public handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body?.name) {
       return badRequest(new MissingParamError('name'))
@@ -20,6 +25,10 @@ export class SignUpController implements Controller {
       return badRequest(new MissingParamError('passwordConfirmation'))
     }
 
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
+    }
     return {
       statusCode: 200,
       body: { message: 'dados do usuário' }
--- src/presentation/errors/missing-param-error.ts
+++ src/presentation/errors/missing-param-error.ts
@@ -1,6 +1,6 @@
 export class MissingParamError extends Error {
   constructor (paramName: string) {
-    super()
-    this.name = `Missing param: ${paramName}`
+    super(`Missing param: ${paramName}`)
+    this.name = 'MissingParamError'
   }
 }
